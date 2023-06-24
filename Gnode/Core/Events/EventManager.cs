29a5cb2481f685d85bb24faea7eefa8bec0a6097
gh9st33 <gh9st33@yahoo.com>
2023-06-24 02:13:04 -0500

    added event code

--- Gnode/Core/Events/EventManager.cs
+++ Gnode/Core/Events/EventManager.cs
@@ -1,14 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gnode.Core.Events
 {
     public class EventManager
     {
-        // Implementation depends on your event model
-    }
+        private Dictionary<Type, List<Delegate>> eventListeners = new Dictionary<Type, List<Delegate>>();
 
+        public void Register<T>(Action<T> listener)
+        {
+            Type eventType = typeof(T);
+            if (!eventListeners.ContainsKey(eventType))
+            {
+                eventListeners[eventType] = new List<Delegate>();
+            }
+            eventListeners[eventType].Add(listener);
+        }
+
+        public void Unregister<T>(Action<T> listener)
+        {
+            Type eventType = typeof(T);
+            if (eventListeners.ContainsKey(eventType))
+            {
+                eventListeners[eventType].Remove(listener);
+            }
+        }
+
+        public void Dispatch<T>(T eventToDispatch)
+        {
+            Type eventType = typeof(T);
+            if (eventListeners.ContainsKey(eventType))
+            {
+                foreach (Delegate listener in eventListeners[eventType])
+                {
+                    listener.DynamicInvoke(eventToDispatch);
+                }
+            }
+        }
+    }
 }
--- Gnode/Core/Events/SignalManager.cs
+++ Gnode/Core/Events/SignalManager.cs
@@ -1,14 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Gnode.Core.Events
 {
     public class SignalManager
     {
-        // Implementation depends on your signal model
-    }
+        private Dictionary<string, List<Action>> signalListeners = new Dictionary<string, List<Action>>();
 
+        public void Register(string signal, Action listener)
+        {
+            if (!signalListeners.ContainsKey(signal))
+            {
+                signalListeners[signal] = new List<Action>();
+            }
+            signalListeners[signal].Add(listener);
+        }
+
+        public void Unregister(string signal, Action listener)
+        {
+            if (signalListeners.ContainsKey(signal))
+            {
+                signalListeners[signal].Remove(listener);
+            }
+        }
+
+        public void Dispatch(string signal)
+        {
+            if (signalListeners.ContainsKey(signal))
+            {
+                foreach (Action listener in signalListeners[signal])
+                {
+                    listener.Invoke();
+                }
+            }
+        }
+    }
 }
