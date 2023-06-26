74b1776ee85595775961be5ea1579962b3f0d8a2
gh9st33 <gh9st33@yahoo.com>
2023-06-26 14:32:29 -0500

    Updated NodeEditorControl

--- Gnode.UI.WPF/NodeEditorControl.xaml.cs
+++ Gnode.UI.WPF/NodeEditorControl.xaml.cs
@@ -2,6 +2,7 @@
 using System.Linq;
 using System.Windows;
 using System.Windows.Controls;
+using System.Windows.Input;
 
 namespace Gnode.UI.WPF
 {
@@ -9,6 +10,7 @@ namespace Gnode.UI.WPF
     {
         private NodeControl selectedNode;
         private PortControl selectedPort;
+        private Point lastMousePosition;
 
         public NodeEditorControl()
         {
@@ -22,8 +24,33 @@ namespace Gnode.UI.WPF
                 Name = "New Node",
                 // Set other properties of the node here
             };
-            node.MouseLeftButtonDown += (s, e) => selectedNode = (NodeControl)s;
+            node.MouseLeftButtonDown += Node_MouseLeftButtonDown;
+            node.MouseMove += Node_MouseMove;
+            node.MouseLeftButtonUp += Node_MouseLeftButtonUp;
             graphControl.AddNode(node);
+        }
+
+        private void Node_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            selectedNode = (NodeControl)sender;
+            lastMousePosition = e.GetPosition(graphControl);
+        }
+
+        private void Node_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (selectedNode != null && e.LeftButton == MouseButtonState.Pressed)
+            {
+                var currentPosition = e.GetPosition(graphControl);
+                var offset = currentPosition - lastMousePosition;
+                Canvas.SetLeft(selectedNode, Canvas.GetLeft(selectedNode) + offset.X);
+                Canvas.SetTop(selectedNode, Canvas.GetTop(selectedNode) + offset.Y);
+                lastMousePosition = currentPosition;
+            }
+        }
+
+        private void Node_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            selectedNode = null;
         }
 
         private void DeleteNode_Click(object sender, RoutedEventArgs e)
