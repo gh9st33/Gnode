3fb4bed67bffdde001ea11f9c5307bf7cbffa17f
gh9st33 <gh9st33@yahoo.com>
2023-06-25 22:40:24 -0500

    Added ConnectionControl

--- Gnode.UI.WPF/ConnectionControl.xaml.cs
+++ Gnode.UI.WPF/ConnectionControl.xaml.cs
@@ -1,28 +1,43 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace Gnode.UI.WPF
 {
-    /// <summary>
-    /// Interaction logic for ConnectionControl.xaml
-    /// </summary>
     public partial class ConnectionControl : UserControl
     {
         public ConnectionControl()
         {
             InitializeComponent();
         }
+
+        public static readonly DependencyProperty IDProperty = DependencyProperty.Register(
+            "ID", typeof(Guid), typeof(ConnectionControl), new PropertyMetadata(Guid.NewGuid()));
+
+        public Guid ID
+        {
+            get { return (Guid)GetValue(IDProperty); }
+            set { SetValue(IDProperty, value); }
+        }
+
+        public static readonly DependencyProperty SourcePortProperty = DependencyProperty.Register(
+            "SourcePort", typeof(PortControl), typeof(ConnectionControl), new PropertyMetadata(null));
+
+        public PortControl SourcePort
+        {
+            get { return (PortControl)GetValue(SourcePortProperty); }
+            set { SetValue(SourcePortProperty, value); }
+        }
+
+        public static readonly DependencyProperty TargetPortProperty = DependencyProperty.Register(
+            "TargetPort", typeof(PortControl), typeof(ConnectionControl), new PropertyMetadata(null));
+
+        public PortControl TargetPort
+        {
+            get { return (PortControl)GetValue(TargetPortProperty); }
+            set { SetValue(TargetPortProperty, value); }
+        }
+
+        // Add more properties and methods for the ConnectionControl here
     }
 }
